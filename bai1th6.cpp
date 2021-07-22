3a813e55e7a556d534b2183dea42ed88ffbbefb4
hoangbuii <60215764+hoangbuii@users.noreply.github.com>
2021-07-22 10:23:23 +0700

    Update bai1th6.cpp

--- bai1th6.cpp
+++ bai1th6.cpp
@@ -18,7 +18,7 @@ int main(){
    count --;
     int min = arr[0], max = arr[0];
     for (int i = 1; i < count; i++){
-        if (max < arr[i]) max = arr[i];
+        if (max < arr[max]) max = arr[i];
         if (min > arr[i]) min = arr[i];
     }
     cout << count << ' ' << min << ' ' << max << ' ' << sum;
